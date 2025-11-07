748112c7fcbd94d250beee0bfa54e7e310e3014e
Mandrake <joxesbueno@gmail.com>
2025-11-07 10:06:40 -0400

    Se agregaron clases sin migrar a los modelos, icono, logo

--- Client/models.py
+++ Client/models.py
@@ -18,4 +18,18 @@ class EscuelaMainConfig(models.Model):
         self.logo.delete()
         super().delete(*args, **kwargs)
 
+class Usuarios(models.Model):
+    nombre = models.CharField(max_length=100, null=False)
+    apellido = models.CharField(max_length=100, null=False)
+    email = models.EmailField(null=False, unique=True)
+    telefono = models.CharField(max_length=20, null=True)
+    fecha_nac = models.DateField(null=True)
+    permisos = models.Choices("estudiante", "profesor", "staff", "administrador")
+    foto_perfil = models.ImageField(upload_to='Users/Images', null=True)
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+    def delete(self, *args, **kwargs):
+        self.foto_perfil.delete()
+        super().delete(*args, **kwargs)
+
 
