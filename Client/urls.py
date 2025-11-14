dc3721c90984121aeabf48ab38a4e7cf70e3cf45
Mandrake <joxesbueno@gmail.com>
2025-11-14 09:05:24 -0400

    Politica de privacidad

--- Client/urls.py
+++ Client/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('inicio/', views.index, name='index'),
+    path('inicio/' or '/', views.index, name='index'),
     path('login/', views.login, name='login'),
+    path('terms/', views.terms, name='terms'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
 ]
--- Client/views.py
+++ Client/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect
 
 # Create your views here.
 
@@ -6,3 +7,7 @@ def index(request):
     return render(request, 'index.html')
 def login(request):
     return render(request, 'login.html')
+def terms(request):
+    return render(request, 'terms.html')
+def privacy_policy(request):
+    return render(request, 'privacy.html')
